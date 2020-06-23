8073b52e56d101985ff67df131c5bc008665fa17
Jan Galiński <jangalinskiphoto@gmail.com>
2020-06-23 22:45:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      todoList: [],
+      newTodoName: '',
+    }
+
+  }
+
+  getAndRenderTodos = () => {
+    axios.get('http://51.75.120.145:3000/todo').then(resp => {
+      this.setState({todoList: resp.data});
+      console.log(resp);
+    })
+  }
+
+  componentDidMount() {
+    this.getAndRenderTodos();
+  }
+
+  deleteAndRenderTodos = id => {
+    axios.delete(`http://51.75.120.145:3000/todo/${id}`).then(() => {
+      this.getAndRenderTodos();
+    })
+  }
+
+  addNewTodoAndRerender = () => {
+    const title = this.state.newTodoName;
+    axios.post('http://51.75.120.145:3000/todo', { title }).then(() => {
+      this.getAndRenderTodos();
+    })
+  }
+
+  onInputChange = event => {
+    this.setState({newTodoName: event.target.value});
+  }
+
+  render() {
+    return (
+      <main>
+        <header>
+          <input onChange={this.onInputChange} type="text"></input>
+          <button onClick={() => {this.addNewTodoAndRerender()}}>DODAJ</button>
+        </header>
+        <ul>
+          {this.state.todoList.map(todo => {
+            return <li key={todo.id}>{todo.title} <button onClick={() => { this.deleteAndRenderTodos(todo.id) }}>USUŃ</button></li>
+          })}
+        </ul>
+      </main>
+    )
+  }
 }
 
 export default App;
